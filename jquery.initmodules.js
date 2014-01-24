72b6f9e70a823eba5fe9c06ab14c46ab1cd63813
Gunnar Hafdal <g@gunn.si>
2014-01-24 14:01:51 +0100

    Changing indentation from 4 spaces to 2 spaces.

--- jquery.initmodules.js
+++ jquery.initmodules.js
@@ -2,41 +2,41 @@
 /*global App, jQuery */
 (function ($) {
 
-    $.fn.initModules = function (namespace, initModuleToCall) {
-        if (typeof (namespace) === 'undefined') { namespace = App; }
-        if (typeof (initModuleToCall) === 'undefined') { initModuleToCall = 'initialize'; }
+  $.fn.initModules = function (namespace, initModuleToCall) {
+    if (typeof (namespace) === 'undefined') { namespace = App; }
+    if (typeof (initModuleToCall) === 'undefined') { initModuleToCall = 'initialize'; }
 
-        // This is so we can use trim in IE8.
-        function _trim() {
-            return this.replace(/^\s+|\s+$/g, '');
+    // This is so we can use trim in IE8.
+    function _trim() {
+      return this.replace(/^\s+|\s+$/g, '');
+    }
+    if (typeof String.prototype.trim !== 'function') {
+      String.prototype.trim = _trim;
+    }
+
+    var initializers = this.find('[data-js-initialize]');
+    for (var i = 0; i < initializers.length; i++) {
+      var $this = $(initializers[i]),
+        modulesList = $this.data('js-initialize');
+
+      var modulesArray = modulesList.trim().split(/\s+/);
+
+      for (var y = 0; y < modulesArray.length; y++) {
+        var module = modulesArray[y];
+
+        if (!namespace[module]) {
+          //console.log("Module not found: " + module);
+          continue;
         }
-        if (typeof String.prototype.trim !== 'function') {
-            String.prototype.trim = _trim;
+
+        try {
+          //console.log("Trying to call: "+module+"."+initModuleToCall);
+          namespace[module][initModuleToCall]($this); //some modules will need the context they are being called from
         }
-
-        var initializers = this.find('[data-js-initialize]');
-        for (var i = 0; i < initializers.length; i++) {
-            var $this = $(initializers[i]),
-                modulesList = $this.data('js-initialize');
-
-            var modulesArray = modulesList.trim().split(/\s+/);
-
-            for (var y = 0; y < modulesArray.length; y++) {
-                var module = modulesArray[y];
-
-                if (!namespace[module]) {
-                    //console.log("Module not found: " + module);
-                    continue;
-                }
-
-                try {
-                    //console.log("Trying to call: "+module+"."+initModuleToCall);
-                    namespace[module][initModuleToCall]($this); //some modules will need the context they are being called from
-                }
-                catch (e) {
-                    //console.error("Failed to call: "+module+"."+initModuleToCall, e);
-                }
-            }
+        catch (e) {
+          //console.error("Failed to call: "+module+"."+initModuleToCall, e);
         }
-    };
+      }
+    }
+  };
 }(jQuery));
